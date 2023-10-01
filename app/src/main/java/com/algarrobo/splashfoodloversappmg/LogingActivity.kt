8a9e2eb762b81418b36912a3d19eff70e9b66bab
Martin Garcia <martingarcia08.00@gmail.com>
2023-10-01 00:24:31 -0500

    loginregisterappMARTING

--- app/src/main/java/com/algarrobo/splashfoodloversappmg/LogingActivity.kt
+++ app/src/main/java/com/algarrobo/splashfoodloversappmg/LogingActivity.kt
@@ -3,9 +3,9 @@ package com.algarrobo.splashfoodloversappmg
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class LoginActivity : AppCompatActivity() {
+class LogingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_loging)
     }
 }
---
+++ app/src/main/java/com/algarrobo/splashfoodloversappmg/Signupcost.kt
@@ -0,0 +1,11 @@
+package com.algarrobo.splashfoodloversappmg
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Signupcost : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signupcost)
+    }
+}
