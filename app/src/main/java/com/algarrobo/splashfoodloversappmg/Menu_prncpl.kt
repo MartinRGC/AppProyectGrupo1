64bf5be2d15677fed48d3c2ee898ea2f420ff3ab
Naydu <naydu.3005@gmail.com>
2023-10-07 10:50:36 -0500

    menu_prncpl

---
+++ app/src/main/java/com/algarrobo/splashfoodloversappmg/Menu_prncpl.kt
@@ -0,0 +1,11 @@
+package com.algarrobo.splashfoodloversappmg
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Menu_prncpl : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu_prncpl)
+    }
+}
