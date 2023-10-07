e249592d8f3b13b77c5a406731523172d388674a
renzovargas04 <20100038@ue.edu.pe>
2023-10-07 11:27:04 -0500

    Agregado

---
+++ app/src/main/java/com/algarrobo/splashfoodloversappmg/IngresarDatos.kt
@@ -0,0 +1,11 @@
+package com.algarrobo.splashfoodloversappmg
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class IngresarDatos : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ingresar_datos)
+    }
+}
---
+++ app/src/main/java/com/algarrobo/splashfoodloversappmg/RedactarAyuda.kt
@@ -0,0 +1,11 @@
+package com.algarrobo.splashfoodloversappmg
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RedactarAyuda : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_redactar_ayuda)
+    }
+}
